f02c04302bafb9b46566caad529188f20bf6da16
Juan Bono <juanbono94@gmail.com>
2017-10-15 20:49:57 -0300

    update dependencies

--- library/Criptografia/AffineLinear.hs
+++ library/Criptografia/AffineLinear.hs
@@ -2,11 +2,3 @@
 {-# LANGUAGE DataKinds #-}
 
 module Criptografia.AffineLinear where
-
-import Numeric.LinearAlgebra
-import Data.Modular
-import qualified Data.Vector as V
-
-import Criptografia.Cipher
-
-aM = matrix 2 [1,2,3,4]
